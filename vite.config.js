d6977a04edd2c1612df25b48887ab071e2f560ec
cesarolvrdz <cesolvrdz@gmail.com>
2025-09-04 00:47:47 -0600

    Initial commit - CV Harvard Style

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CV/' // Asumiendo que el nombre de tu repositorio será 'CV'
+  base: '/CV-de-Ces/' // Nombre del repositorio actual
 })
